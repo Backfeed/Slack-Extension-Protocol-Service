93dcce865664a6aa57a9c978b5f83dc7d334e2bb
shahar halutz <shahar.halutz@gmail.com>
2015-06-03 19:54:22 +0300

    merge- update

--- static/controllers/contributions.js
+++ static/controllers/contributions.js
@@ -1,10 +1,12 @@
 angular.module('MyApp')
-  .controller('ContributionsCtrl', function($scope,$auth,$location,Contributions,ContributionDetail,SaveContribution) {
+  .controller('ContributionsCtrl', function($scope,$auth,$location,$stateParams,Contributions,ContributionDetail,SaveContribution,CloseContribution) {
+	  $scope.contributionId = $stateParams.contributionId;
 	
 	// if not authenticated return to splash:
 	if(!$auth.isAuthenticated()){
 		$location.path('splash'); 
     }
+
 
 	//$scope.slackUsers = Users.getUsers();
   $scope.ContributionModel = {			
@@ -14,11 +16,21 @@ angular.module('MyApp')
 			contributers : [{contributer_id:'',contributer_percentage:''}],
 			intialBid : [{tokens:'',reputation:''}]	
 	};
-  $scope.testModel = {			
-			file : '',
-			id : '',
-			status : ''						
+  $scope.BidModel = {			
+		    tokens : '',
+			owner : '',
+			reputation : '',
+			contribution_id : ''
 	};
+  
+  $scope.ContributionModelForView = {			
+			file : '',
+			owner : '',
+			min_reputation_to_close : '',
+			contributionContributers : [{contributer_id:'',contributer_percentage:''}],
+			bids : [{tokens:'',reputation:''}]	
+	};
+  
   $scope.removeContributionItem = function(index) {
       $scope.ContributionModel.contributers.splice(index, 1);
 };
@@ -28,16 +40,20 @@ angular.module('MyApp')
 			contributionId = entity.id;
 		}
 		console.log("get Contribution "+contributionId);
-		test1 = ContributionDetail.getDetail({contributionId:entity.id});
-		console.log("This is print"+ JSON.stringify(test1));
-		console.log("This is print"+ JSON.stringify(test1).file);		
-		//console.log("query id is : " + queryId);
 		$location.path("/contribution/"+ contributionId);
+			
 	};
+	
 	$scope.createContribution = function(){
 		console.log("Create Contribution");		
 		console.log($scope.ContributionModel);
 		$location.path("/contribution");
+	};
+	$scope.addBid = function(){
+		console.log("Create Bid");		
+		console.log($scope.ContributionModelForView);
+		$scope.BidModel.contribution_id = $scope.ContributionModelForView.id
+		$location.path("/bids");
 	};
 	$scope.contributions = Contributions.getAllContributions();
 	$scope.gridQueryOptions = { 
@@ -49,9 +65,16 @@ angular.module('MyApp')
 	                     ]
 	    };
 
-	if($scope.id && $scope.id != 0){
-		$scope.ContributionModel = ContributionDetail.getDetail({contributionId:$scope.id});
-		console.log($scope.ContributionModel);
+	if($scope.contributionId && $scope.contributionId != 0){
+		$scope.data1 = ContributionDetail.getDetail({contributionId:$scope.contributionId});	
+		$scope.data1.$promise.then(function (result) {
+				$scope.ContributionModelForView = result;
+				console.log("This is "+$scope.ContributionModelForView);
+				console.log("Status is "+$scope.ContributionModelForView.status);
+				console.log("Id is "+$scope.ContributionModelForView.status);
+				console.log("File is "+$scope.ContributionModelForView.file);
+				
+			});	
 	}
 	$scope.addContributer = function() {
 		$scope.ContributionModel.contributers.push({
@@ -64,13 +87,22 @@ angular.module('MyApp')
 	
 	//$scope.users = User.query();
   	$scope.orderProp = "targetName"; // set initial order criteria
-	
 	$scope.submit = function(){
 		console.log("In Submit method");
 		console.log($scope.ContributionModel)
 		$scope.data = SaveContribution.save({},$scope.ContributionModel);
 		$scope.data.$promise.then(function (result) {
 			alert('Successfully saved');
+			$location.path("/contributions");
+		});
+	};
+	
+	$scope.closeContribution = function(){
+		console.log("In closeContribution method");
+		console.log($scope.ContributionModelForView.id)
+		$scope.data = CloseContribution.save({},$scope.ContributionModelForView);
+		$scope.data.$promise.then(function (result) {
+			alert('Contribution closed');
 			$location.path("/contributions");
 		});
 		
