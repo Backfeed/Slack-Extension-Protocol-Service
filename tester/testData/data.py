3f89ce67c71e5944e1a0c927b4af4125ce4f3593
shahar halutz <shahar.halutz@gmail.com>
2015-06-17 18:37:42 +0200

    added comments

--- tester/testData/data.py
+++ tester/testData/data.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 
 # contribution is hard coded to be a single contribution where the first user in the system is the owner
+# NOTE: notice that the contributer_id is according to order of users in users.json
+
 contributers =[
 	{
 		'contributer_id':1,
@@ -9,21 +11,21 @@ contributers =[
 ]
 
 
-
+# NOTE: notice that the owner user Id is according to order of users in users.json
 bids =[
 	{
 		"tokens":60,
 		"reputation":40,
-		"owner":1,  # NOTE: notice that the owner user Id is according to order of users in users.json
-		"contribution_id":1, # there is currently only one contribution hard coded
+		"owner":1,  
+		"contribution_id":1, 
 		"stake":10,
 		"time_created":datetime.now()
 	},
 	{
 		"tokens":40,
 		"reputation":30,
-		"owner":2,  # NOTE: notice that the owner user Id is according to order of users in users.json
-		"contribution_id":1, # there is currently only one contribution hard coded
+		"owner":2,  
+		"contribution_id":1, 
 		"stake":10,
 		"time_created":datetime.now()
 	}
